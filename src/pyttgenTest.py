589cddca82bec366441620006c1c12e3628e0580
ranux <ranux@localhost>
2010-05-14 16:49:58 +0400

    Renaming and refactoring. Added license and readme

--- src/pyttgenTest.py
+++ src/pyttgenTest.py
@@ -1,10 +1,24 @@
+#encoding:utf8
 '''
 Created on 12.05.2010
+Copyright (C) 2010 Alexander S. Razzhivin ( site http://httpbots.com )
 
-@author: Alexander S. Razzhivin
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import unittest
-from truthtable_constructor import *
+from pyttgen import TruthTableConstructor
+from pyttgen import LogicalValue
 
 class Test(unittest.TestCase):
 
